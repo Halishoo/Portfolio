ae393155aa7a94587084baa19308af971a9556ea
Oula Halonen <halishoo@gmail.com>
2015-09-11 15:17:29 +0300

    added some files that were missing

---
+++ src/lomake.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$myfile = fopen("yhteydenotot.txt", "w") or die();
+
+
+fwrite($myfile, "Nimi: " . $_POST["name"] . "\n");
+fwrite($myfile, "Sähköposti: " . $_POST["email"] . "\n");
+fwrite($myfile, "Viesti: " . $_POST["message"] . "\n");
+
+fclose($myfile);
+
+header("Location: index.html");
+
+?>
